d95047a69ea7640e5d3e7eda9ee7b546715813a9
dx0611 <dingxian@my.swjtu.edu.cn>
2024-09-08 22:48:08 +0800

    Update Right.vue

--- src/views/Main/Right.vue
+++ src/views/Main/Right.vue
@@ -21,13 +21,13 @@ const store = mainStore();
 // 站点链接
 const siteUrl = computed(() => {
   const url = import.meta.env.VITE_SITE_URL;
-  if (!url) return "imsyy.top".split(".");
+  if (!url) return "imsyy.top".split(" ");
   // 判断协议前缀
   if (url.startsWith("http://") || url.startsWith("https://")) {
     const urlFormat = url.replace(/^(https?:\/\/)/, "");
-    return urlFormat.split(".");
+    return urlFormat.split(" ");
   }
-  return url.split(".");
+  return url.split(" ");
 });
 </script>
 
